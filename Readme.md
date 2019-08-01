4264cb1ec6e86f4c9805e4ab3520a912ff3d0f9d
fakhrinf <fakhrinf@hotmail.com>
2019-08-02 00:58:13 +0700

    initial commit html view with express basic route

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Vivent
+
+This is a event creator apps. User can create event, join event and send feedback to the event.
+
+This project will use MEVN Stack for future development.
---
+++ app.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/feedback', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/feedback.html'));
+});
+
+app.listen(3000, () => console.log("Server run on port 3000"));
